b70f3b12c9daff7fce558aa2a43b48506972e0c1
beradug22 <beradug22@gmail.com>
2023-10-26 10:47:00 +0530

    First Commmit

---
+++ index.js
@@ -0,0 +1,18 @@
+// console.log("Hello World!");
+
+const fs = require("fs");
+
+// fs.writeFileSync("hello.txt", "Hello from Node.js"); // Create and write to the file
+
+// fs.writeFileSync("hello.txt", "Bhai Bhai from Node.js"); //Overwrite the file
+
+// fs.appendFileSync("hello.txt", "Node js is Everything");  //append to the file
+
+// const buf_data = fs.readFileSync("hello.txt"); //Read the file in to binary data
+
+// console.log(buf_data);//binary data
+
+// const org_data = buf_data.toString(); //Convert binary data to string
+// console.log(org_data);
+
+// fs.renameSync("hello.txt", "Myfile.txt"); //Rename the file
